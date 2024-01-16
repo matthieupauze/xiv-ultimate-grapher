daf976e33ff91f08f3c08b31c9faff7146f5fd2d
Matt <matt.pauze@gmail.com>
2024-01-16 13:18:37 -0500

    update with recent fight data and clean null values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Fight, Logs } from './interfaces';
+import { CleanFight, Fight, Logs } from './interfaces';
 
 const logs: Logs = require('../assets/logs.json');
 
@@ -13,17 +13,23 @@ const DSR_PHASES = 8;
 export class AppComponent {
   // TODO(Matt): Pull logs in from API directly, hopefully with caching mechanism
   formattedLogs = logs.data.reportData.reports.data
+    .map((x) => ({
+      ...x,
+      fights: x.fights.filter((x) => x.bossPercentage != null) as CleanFight[],
+    }))
     .filter((x) => x.fights.length !== 0)
-    .reduce((acc, curr) => {
-      curr.fights.forEach((x) =>
-        acc.push({
-          ...x,
-          bossPercentage: 100 - x.bossPercentage,
-          startTime: x.startTime + curr.startTime,
-        })
-      );
+    .reduce<(Fight & { bossPercentage: number })[]>((acc, curr) => {
+      curr.fights
+        .filter((f) => f.bossPercentage != null)
+        .forEach((x) =>
+          acc.push({
+            ...x,
+            bossPercentage: 100 - x.bossPercentage,
+            startTime: x.startTime + curr.startTime,
+          })
+        );
       return acc;
-    }, [] as Fight[])
+    }, [])
     .sort((a, b) => a.startTime - b.startTime)
     .map((f, i) => [
       i,
--- src/app/interfaces.ts
+++ src/app/interfaces.ts
@@ -1,9 +1,11 @@
 export interface Fight {
   id: number;
   startTime: number;
-  bossPercentage: number; // Lower is better
+  bossPercentage: number | null | undefined; // Lower is better
   lastPhaseAsAbsoluteIndex: number;
 }
+
+export type CleanFight = Fight & { bossPercentage: number };
 
 export interface Logs {
   data: {
